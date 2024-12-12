d1e83521341d2ecf4ae133a7eb06a717561abb1e
JoseWilian <josewilian.work@gmail.com>
2024-12-12 02:59:07 -0300

    first commit

---
+++ trabalho de implementacao sobre representacao de grafos/algoritmo.py
@@ -0,0 +1,44 @@
+import sys
+
+def createSquareMatrix(s):
+    n = [[0 for _ in range(s)] for _ in range(s)]
+    return n
+
+def txtToList(file):
+    m = []
+    with open(file, 'r') as file:
+        for l in file:
+            nums = list(map(int, l.split()))
+            m.append(nums)
+    return m
+
+def listToMatrix(list, matrix, option):
+    # preenche a matriz para representar um grafo não-direcionado
+    if option == "1":
+        for i in list[1:]:
+            temp = i
+            matrix[temp[0]-1][temp[1]-1] = temp[2]
+            matrix[temp[1]-1][temp[0]-1] = temp[2]
+    #preenche a matriz para representar um grafo direcionado
+    if option == "2":
+        for i in list[1:]:
+            temp = i
+            matrix[temp[0]-1][temp[1]-1] = temp[2]
+
+def main():
+    if len(sys.argv) == 3 and (sys.argv[2] == "1" or sys.argv[2] == "2"):
+        file = argumentos = sys.argv[1]
+        values = txtToList(file)
+        size = values[0]
+        size = size[0]
+        matrix = createSquareMatrix(size)
+        listToMatrix(values, matrix, sys.argv[2])
+        
+        for i in matrix:
+            print(i)
+    else:
+        print("\ninsufficient or invalid arguments\n")
+        print("correct example: algoritmo test.g 1\n")
+        exit(0)
+if __name__ == "__main__": 
+    main()
